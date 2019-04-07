b0558dbb4d1002f00a0cfcc2b50a6665391b4507
betsu <bumpchama@gmail.com>
2019-04-08 00:20:18 +0800

    feat:對應 sass 7-1 pattern folder stracture

--- task/watch-css.js
+++ task/watch-css.js
@@ -2,34 +2,35 @@ var nodemon = require('nodemon');
 var glob = require('glob');
 var fs = require('fs');
 var path = require('path');
-var cssFolder = glob.sync('**/css/', { matchBase: true, ignore: ['node_modules/**', 'source-map/**'] });
-var cssFolderIgnore = ['style.css', 'style-edit.css',
-    'style-source.css'
-];
+var sassFolder = glob.sync('**/sass/', {matchBase: true, ignore: ['node_modules/**', 'source-map/**']});
+var cssFolderIgnore = ['style.css', 'style-edit.css', 'style-source.css'];
 
 nodemon({
-    runOnChangeOnly: true,
-    watch: cssFolder,
-    ignore: cssFolderIgnore,
-    ext: 'css scss sass',
-    script: 'task/csscompiled.js',
-    watchOptions: {
-        awaitWriteFinish: true
-    }
-})
+  runOnChangeOnly: true,
+  watch: sassFolder,
+  ignore: cssFolderIgnore,
+  ext: 'css scss sass',
+  script: 'task/csscompiled.js',
+  watchOptions: {
+    awaitWriteFinish: true
+  }
+});
 
-nodemon.once('start', function() {
+nodemon
+  .once('start', function() {
     fs.writeFileSync('task/.changelog', '');
     console.log('watch:css 啟動');
-}).on('restart', function(files) {
-    console.log('[change] ' + getFile(files))
-    fs.writeFileSync('task/.changelog', getFile(files))
-}).on('log', function(logs) {
-    // console.log(logs.colour)
-})
+  })
+  .on('restart', function(files) {
+    console.log('[change] ' + getFile(files));
+    fs.writeFileSync('task/.changelog', getFile(files));
+  })
+  .on('log', function(logs) {
+    // console.log(logs.colour);
+  });
 
-function getFile(files){
-    files = files[0].slice(1);
-    var dirname = process.cwd().slice(1) + '\\';
-    return files.replace(dirname, '');
+function getFile(files) {
+  files = files[0].slice(1);
+  var dirname = process.cwd().slice(1) + '\\';
+  return files.replace(dirname, '');
 }
